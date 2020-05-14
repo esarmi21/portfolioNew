917e2d96a6d232870fc2bb40c17adbcdeb33300c
jay sarmi <esarmi21@gmail.com>
2020-05-13 21:18:30 -0700

    Updates

---
+++ precache-manifest.340104e170ecdd7c5997724888559552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabfc95f25ceab326e83209eddc3b3ef",
+    "url": "/PortfolioNew/index.html"
+  },
+  {
+    "revision": "c9e37d02ffc8fcce4c92",
+    "url": "/PortfolioNew/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a687df4c8ac6df8b5246",
+    "url": "/PortfolioNew/static/js/2.1b4dfd3c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/PortfolioNew/static/js/2.1b4dfd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e37d02ffc8fcce4c92",
+    "url": "/PortfolioNew/static/js/main.2ac706de.chunk.js"
+  },
+  {
+    "revision": "ed5ba379e528af3dc7e5",
+    "url": "/PortfolioNew/static/js/runtime-main.24b716dc.js"
+  }
+]);
